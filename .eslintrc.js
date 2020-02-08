d1b7496859bc03fa0b5cdcd09b357aee0aed018e
reginaldo.andrade <reginaldo.andrade@outlook.com>
2020-02-08 15:52:13 -0300

    [FIX] Adicionando arquivos de configuração do eslint e prettier.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-user-this': 'off',
+    'no-params-reassing': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'import/no-named-as-default': 0,
+    'class-methods-use-this': 'off',
+    camelcase: 'off'
+  }
+};
